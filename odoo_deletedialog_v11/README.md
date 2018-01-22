cd701b8b4baf9b0ecadb8efb5a8f343eac5c4517
Navabrind <venki@navabrindsol.com>
2018-01-23 00:10:47 +0530

    Added Odoo V11 again
    Added Odoo V11 again

---
+++ odoo_deletedialog_v11/README.md
@@ -0,0 +1,4 @@
+Confirm before Delete
+-----------------------------------
+
+A Confirmation dialogue box appears, when clicked on Trash icon button in line items list records.
---
+++ odoo_deletedialog_v11/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Delete Dialog Box',
+    'category': 'Extra Tools',
+    'summary': 'Get a Confirmation dialog box upon deletion of a record',
+    'version': '1.0',
+    'description': """A Confirmation Pop Up dialog box will show upon deletion of a record/row of item""",
+    'author': 'Navabrind IT Solutions',
+    'website': 'https://www.navabrinditsolutions.com',
+    'images':['images/banner_confirmation.png'],
+    'depends': ['base','web'],
+    'data': [
+	'extra.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+	'license': 'AGPL-3'
+}
